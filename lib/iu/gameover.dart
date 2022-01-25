fdbc0e7523b06b4928e16055dccff8ef62cfb21c
pierre <pierre.teodoresco@etu.univ-amu.fr>
2022-01-25 12:22:23 +0100

    ecran de gameover avec affichage leader bord et gagnant

--- lib/iu/gameover.dart
+++ lib/iu/gameover.dart
@@ -1,12 +1,72 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ptut_game/scheduler/player.dart';
+
+import '../delayed_animation.dart';
+import 'game_settings.dart';
 
 class GameOverGUI extends State<GameOverGUIState> {
 
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  Widget build(BuildContext contect){
+    return Scaffold(
+        backgroundColor: Color(0xFFEDECF2),
+        body: SingleChildScrollView(
+          child: Container(
+              margin: EdgeInsets.symmetric(
+                vertical: 60,
+                horizontal: MediaQuery.of(contect).size.width/4,
+              ),
+              child: Column(
+                children:  [
+                  DelayedAnimation(
+                    delay: 500,
+                    child: Container(
+                      height: 150,
+                      child: const FlutterLogo(size:100),
+                    ),
+                  ),
+                  DelayedAnimation(
+                    delay: 1200,
+                    child: Text("Gagnant : " + getWinner()),
+                  ),
+                  DelayedAnimation(
+                      delay: 1200,
+                      child: Table(
+                          border: TableBorder.all(),
+                          children:
+                          getLeaderBoard()
+                      )
+                  ),
+                ],
+              )
+          ),)
+    );
+  }
+
+  String getWinner() {
+    return GameMenuSettings.playerList.reduce((a, b) => a.getPoint() < b.getPoint() ? a : b).getName();
+  }
+
+  List<TableRow> getLeaderBoard() {
+    List<TableRow> widgetChildren = [];
+    widgetChildren.add (const TableRow(
+      children: [
+        Text("Pseudo"),
+        Text("Points"),
+      ],
+    ),);
+
+    List<player> sortedPlayerList = GameMenuSettings.playerList;
+    sortedPlayerList.sort((a,b) => b.getPoint().compareTo(a.getPoint()));
+    for (player p in sortedPlayerList) {
+      widgetChildren.add(TableRow(children: [
+        Text(p.getName()),
+        Text(p.getPoint().toString()),
+      ]));
+    }
+    return widgetChildren;
   }
 
 }
